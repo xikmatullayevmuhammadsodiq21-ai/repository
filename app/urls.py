cdf6e2c47bc2a101e5e31d251e02f06d6e6c305b
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-03 19:21:52 +0500

    fix some porblems

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home')
-    path('', views.contact_page, name='contact')
-    path('', views.our_page, name='our')
-    path('', views.about_page, name='about')
+    path('', views.home_page, name='home'),
+    path('contact/', views.contact_page, name='contact'),
+    path('our/', views.our_page, name='our'),
+    path('about/', views.about_page, name='about'),
 ]
 
