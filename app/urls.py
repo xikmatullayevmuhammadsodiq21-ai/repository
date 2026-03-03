44c3015d820d028ce3902021e4d9ee6aa2a22162
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-03 21:39:49 +0500

    fixes again

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('contact/', views.contact_page, name='contact'),
-    path('our/', views.our_page, name='our'),
-    path('about/', views.about_page, name='about'),
+    path("", views.home, name="home"),
+    path("services/", views.services, name="services"),
+    path("about/", views.about, name="about"),
+    path("testimonials/", views.testimonials, name="testimonials"),
+    path("contact/", views.contact, name="contact"),
 ]
-
