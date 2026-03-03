239bc0ee7e7c8ab506391be73386916f348b9303
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-03 19:18:38 +0500

    kl;ahsdgfhwrefd

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home')
+    path('', views.contact_page, name='contact')
+    path('', views.our_page, name='our')
+    path('', views.about_page, name='about')
 ]
 
