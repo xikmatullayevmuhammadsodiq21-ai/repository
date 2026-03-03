d176bd86c6c729af2f02504039d2f2dfd0394224
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-03 17:30:27 +0500

    ahfjekhsd

--- app/views.py
+++ app/views.py
@@ -6,3 +6,14 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, "index.html")
+
+
+def about_page(request):
+    return render(request, 'about-us.html')
+
+def contact_page(request):
+    return render(request, 'contact-us.html')
+
+
+def our_page(request):
+    return render(request, 'our-services.html')
